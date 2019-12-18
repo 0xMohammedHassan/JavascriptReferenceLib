2d681670e632d86b98e7b301e1ab570d8c9bf539
Mohammed Abo Hassan <38003193+0xMohammedHassan@users.noreply.github.com>
2019-12-18 13:23:58 +0200

    Update lib.js

--- lib.js
+++ lib.js
@@ -5,20 +5,22 @@
 # JavascriptReferenceLib
 Javascript reference library , make the web developer life more easy :)
 
-#Usage:
+# Usage:
+put the JRL file into your JS folder and insert this code at the top of your file to import this lib:
 
-put the JRL file into your JS folder and insert this code at the top of your file to import this lib  
-
+  
 let imported = document.createElement('script');  
-imported.src = 'lib.js';  
-document.head.appendChild(imported);  
+document.body.appendChild(imported);  
+  
+and insert at the end of your body  
+<script  src="lib.js"></script>
 
-after that you can use the functions like that  
+after that you can call the functions like that :  
 numberToString(1234);  
-let str1 = rot13("eretrt");  
-
-
-All rights are reserved.
+let str1 = rot13("eretrt");
+  
+If you found any bug or issue feel free to report it.  
+All rights are reserved.  
 
 ************************************************************
 //****Global variables */
